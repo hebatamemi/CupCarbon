5b01d4e7bef35b7a29ea9f01e526eed036c3427c
hebatamemi <81651768+hebatamemi@users.noreply.github.com>
2021-10-29 17:48:34 +0300

    Create Command_TIMESTAMP.java

---
+++ src/senscript/Command_TIMESTAMP.java
@@ -0,0 +1,34 @@
+package senscript;
+
+import java.time.format.DateTimeFormatter;  
+import java.time.LocalDateTime; 
+
+import device.SensorNode;
+import simulation.WisenSimulation;
+
+public class Command_TIMESTAMP extends Command {
+
+	protected String arg = "" ;
+	
+	public Command_TIMESTAMP(SensorNode sensor, String arg) {
+		this.sensor = sensor ;
+		this.arg = arg ;
+	}
+
+	@Override
+	public double execute() {
+
+                DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+   		LocalDateTime now = LocalDateTime.now();  
+   		String str = dtf.format(now)  
+		WisenSimulation.simLog.add("S" + sensor.getId() + " GET TIMESTAMP.");
+		sensor.getScript().addVariable(arg, str);
+		return 0;
+	}
+
+	@Override
+	public String toString() {
+		return "GET TIMESTAMP";
+	}
+
+}
